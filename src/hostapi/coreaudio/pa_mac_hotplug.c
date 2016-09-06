bcf8be0782ccd5bd30ce485526eb93111b53724e
Evan Balster <evan@imitone.com>
2016-09-06 00:18:20 -0500

    Implemented device change notification

--- src/hostapi/coreaudio/pa_mac_hotplug.c
+++ src/hostapi/coreaudio/pa_mac_hotplug.c
@@ -4,9 +4,51 @@
 
 #include "pa_hotplug.h"
 
+#include "pa_mac_core_internal.h"
 
-void PaUtil_InitializeHotPlug() {}
-void PaUtil_TerminateHotPlug() {}
+typedef struct PaMacCoreHotplugState
+{
+	int dummy;
+}
+	PaMacCoreHotplugState;
+
+static PaMacCoreHotplugState paMacCoreHotplugState;
+
+
+static OSStatus PaMacCoreHardwarePropertyListener
+	(AudioHardwarePropertyID inPropertyID, void *userData)
+{
+	//PaMacCoreHotplugState *hotplugState = (PaMacCoreHotplugState*) userData;
+	
+	//Notify!
+	PaUtil_DevicesChanged(0, NULL);
+	
+	return noErr;
+}
+
+
+void PaUtil_InitializeHotPlug()
+{
+	OSStatus err = noErr;
+	
+	// Add property listener
+	err = AudioHardwareAddPropertyListener(
+		kAudioHardwarePropertyDevices,
+		&PaMacCoreHardwarePropertyListener,
+		&paMacCoreHotplugState);
+	
+	if (err)
+	{
+		VVDBUG(("PaUtil_InitializeHotPlug(): failed to register listener"));
+	}
+}
+void PaUtil_TerminateHotPlug()
+{
+	// Remove property listener
+	AudioHardwareRemovePropertyListener(
+		kAudioHardwarePropertyDevices,
+		&PaMacCoreHardwarePropertyListener);
+}
 
 void PaUtil_LockHotPlug() {}
 void PaUtil_UnlockHotPlug() {}
